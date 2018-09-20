876e5daf0b45c9b01b3b205efd92a23b9a39e740
Chucka, Zachary Tyler <zchucka@ath-allena-lap.gonzaga.edu>
2018-09-20 09:39:33 -0700

    Initial Commit

---
+++ quizFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  quizFun
+//
+//  Created by Chucka, Zachary Tyler on 9/20/18.
+//  Copyright © 2018 Chucka, Zachary Tyler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
